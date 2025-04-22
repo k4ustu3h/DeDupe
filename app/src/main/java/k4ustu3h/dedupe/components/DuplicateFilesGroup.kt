2f3851fae9fdcb5351b212346ad07165c8fdeba3
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-04-23 01:52:15 +0530

    Move file groups to separate Card Views
    Added
      - Logging in FileUtils
    
    Changed
      - Exclude .nomedia file
      - Make the file path as the text for the checkbox
      - Rename DuplicateFileItem to DuplicateFileCard
    
    Removed
      - adapter folder
      - duplicate_item.xml

--- app/src/main/java/k4ustu3h/dedupe/components/DuplicateFilesGroup.kt
+++ app/src/main/java/k4ustu3h/dedupe/components/DuplicateFilesGroup.kt
@@ -3,31 +3,22 @@ package k4ustu3h.dedupe.components
 import com.xwray.groupie.Group
 import com.xwray.groupie.GroupDataObserver
 import com.xwray.groupie.Item
-import k4ustu3h.dedupe.components.item.DuplicateFileItem
+import k4ustu3h.dedupe.components.card.DuplicateFileCard
 import java.io.File
 
 class DuplicateFilesGroup(private val files: List<File>) : Group {
 
-    override fun getItemCount(): Int = files.size
+    override fun getItemCount(): Int = 1
 
-    override fun getItem(position: Int): Item<*> =
-        DuplicateFileItem(files[position], position == 0)
+    override fun getItem(position: Int): Item<*> = DuplicateFileCard(files)
 
-    override fun getPosition(item: Item<*>): Int =
-        files.indexOf((item as DuplicateFileItem).file)
+    override fun getPosition(item: Item<*>): Int = 0
 
     override fun registerGroupDataObserver(observer: GroupDataObserver) {}
 
     override fun unregisterGroupDataObserver(observer: GroupDataObserver) {}
 
     fun getSelectedFiles(): Set<File> {
-        val selectedFiles = mutableSetOf<File>()
-        for (i in 0 until itemCount) {
-            val item = getItem(i) as? DuplicateFileItem
-            if (item?.isSelected() == true) {
-                selectedFiles.add(item.file)
-            }
-        }
-        return selectedFiles
+        return (getItem(0) as? DuplicateFileCard)?.getSelectedFiles() ?: emptySet()
     }
 }
--- app/src/main/java/k4ustu3h/dedupe/utils/FileUtils.kt
+++ app/src/main/java/k4ustu3h/dedupe/utils/FileUtils.kt
@@ -1,5 +1,6 @@
 package k4ustu3h.dedupe.utils
 
+import android.util.Log
 import java.io.File
 import java.io.FileInputStream
 import java.security.MessageDigest
@@ -9,8 +10,10 @@ import kotlin.math.pow
 
 object FileUtils {
 
+    private const val TAG = "FileUtils"
     const val FILE_SIZE_LIMIT_MB = 128L
     const val FILE_SIZE_LIMIT_BYTES = FILE_SIZE_LIMIT_MB * 1024 * 1024
+    private const val NOMEDIA_FILE = ".nomedia"
 
     fun traverseFiles(
         directory: File,
@@ -19,18 +22,37 @@ object FileUtils {
     ) {
         val files = directory.listFiles()
 
-        if (files == null) return
+        if (files == null) {
+            Log.w(TAG, "listFiles returned null for directory: ${directory.absolutePath}")
+            return
+        }
 
         for (file in files) {
+            Log.v(TAG, "Processing file/directory: ${file.absolutePath}")
+            if (file.name == NOMEDIA_FILE) {
+                Log.v(TAG, "Skipping .nomedia file: ${file.absolutePath}")
+                continue
+            }
             if (file.isDirectory) {
+                Log.v(TAG, "Traversing subdirectory: ${file.absolutePath}")
                 traverseFiles(file, fileMap, applySizeLimit)
             } else {
                 if (applySizeLimit && file.length() > FILE_SIZE_LIMIT_BYTES) {
+                    Log.v(
+                        TAG,
+                        "Skipping file due to size limit: ${file.absolutePath} (${getFileSize(file)})"
+                    )
                     continue
                 }
                 val hash = calculateFileHash(file)
                 if (hash != null) {
                     fileMap.getOrPut(hash) { mutableListOf() }.add(file)
+                    Log.d(
+                        TAG,
+                        "File added to map (hash: $hash): ${file.absolutePath} (${getFileSize(file)})"
+                    )
+                } else {
+                    Log.e(TAG, "Could not calculate hash for file: ${file.absolutePath}")
                 }
             }
         }
@@ -48,7 +70,7 @@ object FileUtils {
             val hashBytes = digest.digest()
             return bytesToHex(hashBytes)
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.e(TAG, "Error calculating hash for ${file.absolutePath}: ${e.message}")
             return null
         }
     }
